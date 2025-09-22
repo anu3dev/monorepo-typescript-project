929c05b261a436c0edd4a669a397463ed68f13d0
anurag git <anurag@git>
2025-09-23 03:35:22 +0530

    initial setup

--- packages/designTokens/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Design Tokens Package - Main export file
-// This file provides paths to design token files for use across the EMS system
-
-export const designTokens = {
-  default: './_variablesDefault.scss',
-} as const;
-
-export type ThemeVariant = keyof typeof designTokens;
