8d3a30d2165553118f70b8453fde6bb64e616ec6
anurag git <anurag@git>
2025-09-23 04:06:40 +0530

    initial setup

--- apps/registration-app/src/App.tsx
+++ apps/registration-app/src/App.tsx
@@ -1,10 +1,5 @@
-import React, { useEffect } from 'react';
 import RegistrationApp from './RegistrationApp';
 
 export default function App() {
-  useEffect(() => {
-    document.documentElement.style.setProperty('--ems-primary', '#f59e0b');
-  }, []);
-
   return <RegistrationApp />;
 }
